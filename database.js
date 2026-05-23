ddc119ec6faa03515e9d6dac6f5a7731c1ef3cd6
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2026-05-23 18:22:21 +0530

    mongodb connection

---
+++ database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const connectDb = async ()=>{
+    try{
+        const connectionInstance= await mongoose.connect('mongodb://127.0.0.1:27017/backenddb')
+        console.log(`connection successfull  ${connectionInstance.connection.host}`)
+    }
+    catch(error){
+        console.log(`connection failed ${error}`)
+    }
+    
+}
+
+export default connectDb
