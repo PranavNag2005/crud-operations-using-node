3584ab8a931a27c75fd2e981c79e5f667d52daae
dskvamsi <dskvamsi24@gmail.com>
2026-05-24 13:00:17 +0530

    refactor database connection URI to specify backend database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 const connectDb = async ()=>{
     try{
-        const connectionInstance= await mongoose.connect("mongodb://localhost:27017")
+        const connectionInstance= await mongoose.connect("mongodb://127.0.0.1:27017/backenddb")
         console.log(`connection successfull  ${connectionInstance.connection.host}`)
     }
     catch(error){
